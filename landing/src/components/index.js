0efe863d4ccd4bc6e4f21b50a4e3f8f3a0d8aeca
Daniel Zapata Correa <zapata0423@gmail.com>
2024-08-11 08:40:10 -0500

    React funcionality

--- landing/src/components/index.js
+++ landing/src/components/index.js
@@ -1,3 +1,3 @@
+export * from './Header/Header'
 export * from './Body/Body'
 export * from './Footer/Footer'
-export * from './Header/Header'
--- landing/src/main.jsx
+++ landing/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './style.css'
+import './styles.css'
 
 import App from './App'
 
